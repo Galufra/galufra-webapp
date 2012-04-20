c2225a5f5865a20ce3f6746cfe17d78fbdef6ae6
lou1306 <lucadistefano90@gmail.com>
2012-04-20 12:09:04 +0200

    Adesso utente.php dovrebbe avere un aspetto decente :P

--- galufra-webapp/utente.php
+++ galufra-webapp/utente.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'login/controller/dbController.php';
+require_once 'login/controller/mysqlController.php';
 
 class utente {
 	private $id;
@@ -8,13 +8,30 @@ class utente {
 		$this->id = $i;
 	}
 	
+	/* Query($attr)
+	 * cerca il valore del campo $attr
+	 * nel record con id_utente pari a quello
+	 * dell'oggetto ($this->id). Per farlo crea
+	 * un oggetto mysqlController.
+	 * Una query fallita restituisce false.
+	 */
 	private function Query($attr){
+		$attr = mysql_real_escape_string($attr);
 		$query = "SELECT ". $attr ." FROM utente
 				  WHERE utente.id_utente =". $this->id.";"
+		$db = new mysqlController();
+		$db->connect();
+		$result = $db->makeQuery($query);
+		$db->close();
+		/* Query riuscita: restituzione
+		 * del valore di $attr
+		 */
 		if($result[0]){
 			$record = mysql_fetch_array($result[1]);
 			return $record[$attr];
 		}
+		/* Query fallita: restituisce false
+		 */
 		else 
 			return false;
 	}
